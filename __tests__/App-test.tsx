b51dd68dbdef0108ba22f7ccdebb8d0e10785036
James Zhang <liulfbaby@gmail.com>
2021-03-12 14:54:19 +0800

    feat: add ts-jest and all thins is right

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -7,9 +7,20 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import {render} from '@testing-library/react-native';
+import {render, cleanup} from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  const {getByText} = render(<App />);
-  expect(getByText('Step One')).toBeTruthy();
+describe('App', () => {
+  afterEach(cleanup);
+
+  it('renders correctly', () => {
+    const {getByText} = render(<App />);
+    expect(getByText('Step One')).toBeTruthy();
+  });
+
+  it('should check types of tests', function () {
+    const add = (num1: number) => {
+      return num1 * 2;
+    };
+    expect(add('2')).toBe(4);
+  });
 });
