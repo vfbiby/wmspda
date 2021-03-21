46ea705493432f1913e1a2051af67fa8ac8f293e
James Zhang <liulfbaby@gmail.com>
2021-03-22 01:01:58 +0800

    feat: make should display home page if user is logined pass by faking login

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, Button, TextInput} from 'react-native';
 
 export const App = () => {
-  return (
+  const [isLogin, setIsLogin] = useState(false);
+
+  return isLogin ? (
+    <View>
+      <Text>15336551131</Text>
+    </View>
+  ) : (
     <View>
       <Text>Username</Text>
       <TextInput onChangeText={() => {}} placeholder="Username" />
       <Text>Password</Text>
       <TextInput onChangeText={() => {}} placeholder="Password" />
-      <Button title="Submit" onPress={() => {}} />
+      <Button
+        title="Submit"
+        onPress={() => {
+          setIsLogin(true);
+        }}
+      />
     </View>
   );
 };
