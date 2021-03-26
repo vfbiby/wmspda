c9a41d52774203cbea8215bfb623fbcc9497335d
James Zhang <liulfbaby@gmail.com>
2021-03-26 19:27:04 +0800

    feat: add mock to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
 import Login, {ILoginForm} from './pages/Login';
+import unmock from 'unmock'; // ES6
+
+unmock.on();
+unmock
+  .nock('http://t.test.com')
+  .get('/api/users')
+  .reply(200, {name: 'biby'});
 
 export const App = () => {
   const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState<{name: string}>();
+
+  useEffect(() => {
+    fetch('http://t.test.com/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUser(json);
+      });
+  }, []);
 
   const handleLogin = (user: ILoginForm) => {
     !!user;
@@ -14,6 +30,7 @@ export const App = () => {
     <SafeAreaView>
       <View>
         <Text>15336551131</Text>
+        <Text>{user?.name}</Text>
       </View>
     </SafeAreaView>
   ) : (
